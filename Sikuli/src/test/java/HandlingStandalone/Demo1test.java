067812adafae51294e4d5dff383a6d55d4566cf1
pavanramraopatil <142868670+pavanramraopatil@users.noreply.github.com>
2023-08-31 00:11:23 +0530

    Update Demo1test.java

--- Sikuli/src/test/java/HandlingStandalone/Demo1test.java
+++ Sikuli/src/test/java/HandlingStandalone/Demo1test.java
@@ -1,38 +1,38 @@
 package HandlingStandalone;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.edge.EdgeDriver;
+// import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Demo1test {
 
-WebDriver driver=null;
+//WebDriver driver=null;
 	
 	@Test
 	public void d1Test()
 	{
 		System.out.println("Hi");
-		//WebDriver driver=new ChromeDriver();
-		//driver.get("https://www.flipkart.com/");
-		String BROWSER = System.getProperty("browser");
-		String URL = System.getProperty("url");
+		// //WebDriver driver=new ChromeDriver();
+		// //driver.get("https://www.flipkart.com/");
+		// String BROWSER = System.getProperty("browser");
+		// String URL = System.getProperty("url");
 		
-		if(BROWSER.equalsIgnoreCase("chrome"))
-		{
-			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver();
+		// if(BROWSER.equalsIgnoreCase("chrome"))
+		// {
+		// 	WebDriverManager.chromedriver().setup();
+		// 	driver=new ChromeDriver();
 			
-		}else if(BROWSER.equalsIgnoreCase("firefox"))
-		{
-			WebDriverManager.firefoxdriver().setup();
-			driver=new FirefoxDriver();
-		}else {
-			driver=new ChromeDriver();
-		}
-		driver.get(URL);
+		// }else if(BROWSER.equalsIgnoreCase("firefox"))
+		// {
+		// 	WebDriverManager.firefoxdriver().setup();
+		// 	driver=new FirefoxDriver();
+		// }else {
+		// 	driver=new ChromeDriver();
+		// }
+		// driver.get(URL);
 	}}
 //pull back
